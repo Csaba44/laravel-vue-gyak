3c1277963c71308273df9ca276deb0d3ef99dbc0
Csaba44 <csabi16sheldon@gmail.com>
2025-11-18 18:02:14 +0100

    Laravel sanctum auth

---
+++ api/app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Cache\Repository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $registerUserData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $registerUserData['name'],
+            'email' => $registerUserData['email'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'User created'
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $loginUserData = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::where('email', $loginUserData['email'])->first();
+
+        if (!$user || !Hash::check($loginUserData['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    function logout()
+    {
+        $user = auth('sanctum')->user();
+        
+        $user->tokens()->delete();
+
+        return response()->json([
+            'message' => 'logged out'
+        ]);
+    }
+}
