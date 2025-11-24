4b01dda51b9f918b3eeaa5f2b01f99a1332fa74f
Csaba44 <csabi16sheldon@gmail.com>
2025-11-24 11:16:44 +0100

    Fix: optimizatins, follwing best practices

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-const EXCLUDE_NAV = ['/login', '/register'];
+const EXCLUDE_NAV = ['login', 'register'];
 </script>
 
 <template>
-  <nav v-show="!EXCLUDE_NAV.includes($route.fullPath)">
+  <nav v-show="!EXCLUDE_NAV.includes($route.name)">
     <RouterLink to="/">Go to Home</RouterLink>
     <br>
     <RouterLink to="/login">Go to About</RouterLink>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// Eager loaded routes
 import HomeView from "./views/HomeView.vue";
+
+// Lazy loaded routes
+const LoginView = () => import("./views/LoginView.vue");
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-  { path: "/login", name: "login", component: import("./views/LoginView.vue") }, // Lazy loading
+  { path: "/login", name: "login", component: LoginView },
 ];
 
 const router = createRouter({
